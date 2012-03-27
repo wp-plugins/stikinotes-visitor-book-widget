2700e3c7837b16fe15fa340b912d5bd2da12eccb
chidge <chidge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-27 08:47:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stikinotes-visitor-book-widget/trunk@524213 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+Plugin Name: Stikinotes Widget
+Plugin URI: http://www.stikinotes.com
+Description: Plugin to display a Stikinotes widget on your wordpress website.
+Version: 1.0
+Author: Gareth Chidgey, Ahoy Creative
+Author URI: http://www.ahoycreative.co.uk
+License: GPL2
+*/
+
+
+// Enqueus internal jQuery and stikinotes Javascript
+function stiki_load_scripts()
+{
+        wp_deregister_script( 'jquery' );
+        wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+        wp_enqueue_script( 'jquery' );
+        wp_enqueue_script( 'stiki_js' , 'http://www.stikinotes.com/resources/js/stikishare.js');
+}
+
+function stiki_register_widget()
+{
+    register_widget('stiki_widget');
+}
+
+class stiki_widget extends WP_Widget
+{
+    function stiki_widget()
+    {
+        $this->WP_Widget( 'stiki_widget', 'Stikinotes Widget',
+                            array( 'classname' => 'stikinotes_widget', 'description' => __('Stikinotes Widget', "stiki_widget") ),
+                            array( 'width' => 200, 'height' => 250, 'id_base' => 'stiki_widget' )
+                            );
+    }
+    
+    function widget( $args, $instance )
+    {
+        extract( $args );
+        echo $before_widget;
+        echo '<div class="stikishare" locId="' . $instance['stiki_id'] . '" locName="" imageType="' . $instance['stiki_type'] . '" ></div>';
+        echo $after_widget;
+    }
+    
+    function form( $instance )
+    {
+        $instance = wp_parse_args( (array) $instance, array('title' => __("", "stiki_title"), 'tabindex' => '1') );
+        $selected = '';
+        if ($instance['stiki_type'] == 1)
+        {
+            $selected1 = ' selected="selected"';
+            $selected2 = '';
+        }
+        else if ($instance['stiki_type'] == 2)
+        {
+            $selected1 = '';
+            $selected2 = ' selected="selected"';
+        }
+        echo '
+             <p>
+                <h3>Your Stikiplace ID</h3>
+                <input id="' . $this->get_field_id( 'stiki_id' ) . '" name="' . $this->get_field_name( 'stiki_id' ) . '" value="' . $instance['stiki_id'] . '" style="width:90%;" />
+            </p>
+            <p>
+                <h3>Select Widget</h3>
+                <select id="' . $this->get_field_id( 'stiki_type' ) . '" name="' . $this->get_field_name( 'stiki_type' ) . '" style="width:90%;">
+                    <option value="1" ' . $selected1 . '>Visitor Book</option>
+                    <option value="2" ' . $selected2 . '>Stikinotes Icon</option>
+                </select>
+            </p>
+
+        ';
+    }
+}
+add_action('wp_enqueue_scripts', 'stiki_load_scripts');
+add_action('widgets_init','stiki_register_widget');
+
+?>
