7d1e1889cd813ee75510f289814c43058c344483
chidge <chidge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-27 08:54:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stikinotes-visitor-book-widget/trunk@524217 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stikinotes-visitor-book-widget/plugin.php
+++ stikinotes-visitor-book-widget/plugin.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Stikinotes Widget
 Plugin URI: http://www.stikinotes.com
-Description: Plugin to display a Stikinotes widget on your wordpress website.
+Description: Plugin to display a Stikinotes widget on your wordpress website. Stikinotes is the Global Visitor book, allowing your customers to post reviews and pictures directly to your visitor book from their mobile phones. See www.stikinotes.com/business.aspx for more information.
 Version: 1.0
 Author: Gareth Chidgey, Ahoy Creative
 Author URI: http://www.ahoycreative.co.uk
